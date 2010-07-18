efcef0f0edb5f95df0a68098374509e7e58585e0
Peteris Krumins <peteris.krumins@gmail.com>
2010-07-18 11:39:48 +0300

    better notation

--- jpeg.cc
+++ jpeg.cc
@@ -3,6 +3,7 @@
 #include <jpeglib.h>
 #include <cstdlib>
 #include <cstring>
+#include <cassert>
 
 using namespace v8;
 using namespace node;
@@ -13,15 +14,21 @@ VException(const char *msg) {
     return ThrowException(Exception::Error(String::New(msg)));
 }
 
-typedef enum { BUF_RGBA, BUF_RGB } buf_type_t;
+static inline bool
+str_eq(const char *s1, const char *s2)
+{
+    return strcmp(s1, s2) == 0;
+}
+
+typedef enum { BUF_RGB, BUF_BGR, BUF_RGBA, BUF_BGRA } buffer_type;
 
 class Jpeg : public ObjectWrap {
 private:
-    int width_;
-    int height_;
-    int quality_;
-    buf_type_t buf_type_;
-    Buffer *rgba_;
+    int width;
+    int height;
+    int quality;
+    buffer_type buf_type;
+    Buffer *data;
 
 public:
     static void
@@ -35,21 +42,23 @@ public:
         target->Set(String::NewSymbol("Jpeg"), t->GetFunction());
     }
 
-    Jpeg(Buffer *rgba, int width, int height, int quality, buf_type_t buf_type) :
-        rgba_(rgba), width_(width), height_(height), quality_(quality),
-        buf_type_(buf_type) {}
+    Jpeg(Buffer *ddata, int wwidth, int hheight, int qquality, buffer_type bbuf_type) :
+        data(ddata), width(wwidth), height(hheight), quality(qquality),
+        buf_type(bbuf_type) {}
 
     static unsigned char *
-    rgba_to_rgb(unsigned char *rgba, int rgba_size)
+    rgba_to_rgb(const unsigned char *rgba, int rgba_size)
     {
-        int rgb_size = rgba_size/4*3;
-        unsigned char *rgb = (unsigned char *)malloc(sizeof(unsigned char)*rgb_size);
+        assert(rgba_size%4==0);
+
+        int rgb_size = rgba_size*3/4;
+        unsigned char *rgb = (unsigned char *)malloc(sizeof(*rgb)*rgb_size);
         if (!rgb) return NULL;
-        int i, j;
-        for (i=0,j=0;i<rgba_size;i+=4,j+=3) {
-            rgb[j] = *(rgba+i);
-            rgb[j+1] = *(rgba+i+1);
-            rgb[j+2] = *(rgba+i+2);
+
+        for (int i=0,j=0;i<rgba_size;i+=4,j+=3) {
+            rgb[j] = rgba[i];
+            rgb[j+1] = rgba[i+1];
+            rgb[j+2] = rgba[i+2];
         }
         return rgb;
     }
@@ -68,26 +77,26 @@ public:
         jpeg_create_compress(&cinfo);
         jpeg_mem_dest(&cinfo, &mem_dest, &outsize);
 
-        cinfo.image_width = width_;
-        cinfo.image_height = height_;
+        cinfo.image_width = width;
+        cinfo.image_height = height;
         cinfo.input_components = 3;
         cinfo.in_color_space = JCS_RGB;
 
         jpeg_set_defaults(&cinfo);
-        jpeg_set_quality(&cinfo, quality_, TRUE);
+        jpeg_set_quality(&cinfo, quality, TRUE);
         jpeg_start_compress(&cinfo, TRUE);
 
         unsigned char *rgb_data;
-        if (buf_type_ == BUF_RGBA) {
-            rgb_data = rgba_to_rgb((unsigned char *)rgba_->data(), width_*height_*4);
+        if (buf_type == BUF_RGBA) {
+            rgb_data = rgba_to_rgb((unsigned char *)data->data(), width*height*4);
             if (!rgb_data) return VException("malloc failed in rgba_to_rgb.");
         } else {
-            rgb_data = (unsigned char *)rgba_->data();
+            rgb_data = (unsigned char *)data->data();
         }
 
         JSAMPROW row_pointer;
         while (cinfo.next_scanline < cinfo.image_height) {
-            row_pointer = &rgb_data[cinfo.next_scanline*3*width_];
+            row_pointer = &rgb_data[cinfo.next_scanline*3*width];
             jpeg_write_scanlines(&cinfo, &row_pointer, 1);
         }
 
@@ -95,7 +104,7 @@ public:
         jpeg_destroy_compress(&cinfo);
 
         free(mem_dest);
-        if (buf_type_ == BUF_RGBA) free(rgb_data);
+        if (buf_type == BUF_RGBA) free(rgb_data);
 
         return scope.Close(Encode((char *)mem_dest, outsize, BINARY));
     }
@@ -117,7 +126,7 @@ protected:
         if (!args[3]->IsInt32())
             return VException("Fourth argument must be integer quality.");
         if (!args[4]->IsString())
-            return VException("Fifth argument must be a string. Either 'rgba' or 'rgb'.");
+            return VException("Fifth argument must be a string. Either 'rgb' or 'rgba'.");
 
         int w = args[1]->Int32Value();
         int h = args[2]->Int32Value();
@@ -126,12 +135,19 @@ protected:
 
         if (quality < 0 || quality > 100)
             return VException("Quality must be between 0 and 100 (inclusive).");
-        if (!(strcmp(*bt, "rgb") == 0 || strcmp(*bt, "rgba") == 0))
-            return VException("Fifth argument must be either 'rgba' or 'rgb'.");
-        buf_type_t buf_type = (strcmp(*bt, "rgb") == 0) ?  BUF_RGB : BUF_RGBA;
+        if (!(str_eq(*bt, "rgb") || str_eq(*bt, "rgba")))
+            return VException("Fifth argument must be either 'rgb' or 'rgba'.");
 
-        Buffer *rgba = ObjectWrap::Unwrap<Buffer>(args[0]->ToObject());
-        Jpeg *jpeg = new Jpeg(rgba, w, h, quality, buf_type);
+        buffer_type buf_type;
+        if (str_eq(*bt, "rgb"))
+            buf_type = BUF_RGB;
+        else if (str_eq(*bt, "rgba"))
+            buf_type = BUF_RGBA;
+        else 
+            return VException("Buffer type wasn't 'rgb' or 'rgba'");
+
+        Buffer *data_buf = ObjectWrap::Unwrap<Buffer>(args[0]->ToObject());
+        Jpeg *jpeg = new Jpeg(data_buf, w, h, quality, buf_type);
         jpeg->Wrap(args.This());
         return args.This();
     }
